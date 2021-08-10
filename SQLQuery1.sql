95f4f5018a772a9e792ceb1bbc65ca5914a54316
LucasSarri <lucassarri@hotmail.com>
2021-08-09 21:34:09 -0400

    alteracoes dia 09/08

---
+++ SQLQuery1.sql
@@ -0,0 +1,4 @@
+/*Criando via código o banco lucas*/
+create database lucas;
+/*Apagando via código o banco Teste*/
+drop database Teste;
---
+++ SQLQuery2.sql
@@ -0,0 +1,11 @@
+/*Modelo de select*/
+SELECT coluna1, coluna2 FROM tabela;
+/*retorna todas as colunas da tabela*/
+SELECT * FROM tabela
+
+/*Exemplos*/
+SELECT * FROM person.Person;
+
+SELECT	Title from person.Person;
+
+SELECT firstname, lastname from person.Person;
